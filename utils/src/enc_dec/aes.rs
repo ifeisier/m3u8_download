46ae79290cc8da1290674fbae461527c82a76dd0
ifeisier <ifeisier@hotmail.com>
2024-12-15 23:57:23 +0800

    修改结构体名字Aes128Cbc

--- utils/src/enc_dec/aes.rs
+++ utils/src/enc_dec/aes.rs
@@ -8,13 +8,13 @@ use aes::Aes128;
 // type Aes128CbcEnc = cbc::Encryptor<Aes128>;
 
 /// 解密
-type _Aes128CbcDec = cbc::Decryptor<Aes128>;
+type Aes128CbcDec = cbc::Decryptor<Aes128>;
 
-pub struct Aes128CbcDec;
-impl Aes128CbcDec {
+pub struct Aes128Cbc;
+impl Aes128Cbc {
     // https://docs.rs/cbc/0.1.2/cbc/index.html
     pub fn dec(encrypted_data: &mut [u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
-        let cipher = _Aes128CbcDec::new_from_slices(key, iv)
+        let cipher = Aes128CbcDec::new_from_slices(key, iv)
             .map_err(|e| format!("创建解密器失败: {}", e))
             .unwrap();
 
